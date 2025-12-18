b30c001ebc7bde3f1102a07f7a56275735da6b45
eygwak <gwak.ey@gmail.com>
2025-12-18 13:54:50 +0900

    Initial Commit

---
+++ VoiceQuiz/VoiceQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceQuizApp.swift
+//  VoiceQuiz
+//
+//  Created by 곽은영 on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
